c899928f9f002471b3ea3d86d2bf6b4a73ccf54e
CrackTheCode016 <ibnbassem@gmail.com>
2020-12-09 11:42:09 -0500

    add utf8 flag support

--- patch.js
+++ patch.js
@@ -1,21 +1,21 @@
-var fs = require('fs');
+var fs = require("fs");
 
 function patchPackageJSON() {
-const packageJsonPath = "./package.json";
-fs.readFile(packageJsonPath, (err, data) => {
-    if(err) {
-        console.log(err);
+  const packageJsonPath = "./package.json";
+  fs.readFile(packageJsonPath, (err, data) => {
+    if (err) {
+      console.log(err);
     }
     var json = JSON.parse(data);
-    json.browser = { "fs": false, "tls": false, "net": false }
+    json.browser = { fs: false, tls: false, net: false, utf8: false };
     fs.writeFileSync(packageJsonPath, JSON.stringify(json));
-});
-
+  });
 }
 
 function patchWebPackConfig() {
-    const webpackPath = "node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
-     const webpackPatch = `
+  const webpackPath =
+    "node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
+  const webpackPatch = `
      node: {
          crypto: true,
          http: true,
@@ -24,15 +24,14 @@ function patchWebPackConfig() {
          ws: true
      }`;
 
-    fs.readFile(webpackPath, (err, data) => {
-        if(err) {
-            console.log(err)
-        }
-        const dataStr = data.toString('utf8');
-        var replace = dataStr.replace('node: false,', webpackPatch);
-        fs.writeFileSync(webpackPath, replace);
-    });
-
+  fs.readFile(webpackPath, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    const dataStr = data.toString("utf8");
+    var replace = dataStr.replace("node: false,", webpackPatch);
+    fs.writeFileSync(webpackPath, replace);
+  });
 }
 
 patchWebPackConfig();
